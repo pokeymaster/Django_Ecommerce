97eeec668c9024b3adde5201d34f9f9a492d652c
pokeymaster <cyd1995@hotmail.com>
2024-09-26 02:04:41 +1000

    add account model

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_ecommerce.wsgi.application'
 
+AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases

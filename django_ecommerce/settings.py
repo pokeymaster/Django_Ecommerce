511dffd0bd23885b798ec8977306eaf4c7af90e7
pokeymaster <cyd1995@hotmail.com>
2024-09-26 12:16:43 +1000

    add admin page for product and category

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'category',
     'accounts',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +124,10 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'django_ecommerce/static',
 ]
+
+#media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
